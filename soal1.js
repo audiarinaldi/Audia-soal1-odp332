e27ab71ac2348690133aae7cdfb4a18b4a44ee20
audiarinaldi <audiamaharani02@gmail.com>
2025-06-22 11:52:38 +0700

    submit tugas

--- soal1.js
+++ soal1.js
@@ -1,17 +1,120 @@
 // Jumlahkan semua angka genap dalam array berikut
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-let resultOne
+let resultOne = 0
+for (let i = 0; i < numbers.length; i++) {
+    if(numbers[i] % 2 === 0){
+        resultOne += numbers[i]
+    }
+}
+console.log(resultOne, "<=== RESULT SOAL PERTAMA"); //result = 20
+
+// ITERASI 1:
+// i = 0
+// numbers[0] = 1
+// Cek: 1 % 2 = 1 (GANJIL)
+// Kondisi if tidak terpenuhi → skip
+// resultOne tetap = 0
+
+// ITERASI 2:
+// i = 1
+// numbers[1] = 2
+// Cek: 2 % 2 = 0 (GENAP) ✓
+// Kondisi if terpenuhi → resultOne = 0 + 2 = 2
+// resultOne sekarang = 2
+
+// ITERASI 3:
+// i = 2
+// numbers[2] = 3
+// Cek: 3 % 2 = 1 (GANJIL)
+// Kondisi if tidak terpenuhi → skip
+// resultOne tetap = 2
+
+// ITERASI 4:
+// i = 3
+// numbers[3] = 4
+// Cek: 4 % 2 = 0 (GENAP) ✓
+// Kondisi if terpenuhi → resultOne = 2 + 4 = 6
+// resultOne sekarang = 6
+
+// ITERASI 5:
+// i = 4
+// numbers[4] = 5
+// Cek: 5 % 2 = 1 (GANJIL)
+// Kondisi if tidak terpenuhi → skip
+// resultOne tetap = 6
 
 
 const numbersTwo = [1, 2, 3, 4, 5, 6, 7, 8];
 //Tambahkan semua angka ganjil dalam array dan kembalikan hasilnya.
-let resultTwo
+let resultTwo = 0
+for (let i = 0; i < numbersTwo.length; i++) {
+    if(numbers[i] % 2 !== 0){
+        resultTwo += numbers[i]
+    }
+}
+console.log(resultTwo, "<=== RESULT SOAL KEDUA"); // result 16
+
+// ITERASI 1:
+// i = 0
+// numbers[0] = 1
+// Cek: 1 % 2 = 1 (GANJIL)
+// Kondisi if terpenuhi → resultTwo = 0 + 1 = 1
+// resultTwo sekarang = 1
+
+// ITERASI 2:
+// i = 1
+// numbers[1] = 2
+// Cek: 2 % 2 = 0 (GENAP) ✓
+// Kondisi if tidak terpenuhi → skip
+// resultTwo tetap = 1
+
+// ITERASI 3:
+// i = 2
+// numbers[2] = 3
+// Cek: 3 % 2 = 1 (GANJIL)
+// Kondisi if terpenuhi → resultTwo = 1 + 3 = 4
+// resultTwo sekarang = 4
+
+// ITERASI 4:
+// i = 3
+// numbers[3] = 4
+// Cek: 4 % 2 = 0 (GENAP) ✓
+// Kondisi if tidak terpenuhi → skip
+// resultTwo tetap = 4
+
+// ITERASI 5:
+// i = 4
+// numbers[4] = 5
+// Cek: 5 % 2 = 1 (GANJIL)
+// Kondisi if terpenuhi → resultTwo = 4 + 5 = 9
+// resultTwo sekarang = 9
 
 
 // Ubah semua huruf kecil dalam array menjadi huruf besar
 let students = ["Tito", "arY", "Bostang"]
-let resultThree
+let resultThree = []
 
+for (let i = 0; i < students.length; i++) {
+    let studentUpper = students[i].toUpperCase()
+    resultThree.push(studentUpper)
+}
+
+console.log(resultThree, "<=== RESULT SOAL KETIGA")
+
+// ITERASI 1
+// i = 0
+// studentUpper = Tito.toUpperCase = TITO 
+// resultThree = ["TITO"]
+
+// ITERASI 2
+// i = 1
+// studentUpper = arY.toUpperCase = ARY 
+// resultThree = ["TITO", "ARY"]
+
+// ITERASI 3
+// i = 2
+// studentUpper = Bostang.toUpperCase = BOSTANG 
+// resultThree = ["TITO", "ARY", "BOSTANG"]
 
 // Ambil hanya nama-nama orang yang umurnya di atas 18 tahun.
 // Ambil hanya orang yang panjang namanya lebih dari 5
@@ -22,6 +125,50 @@ const people = [
     { name: 'Cici', age: 17 }
 ];
 
+let resultFour = []
+
+for (let i = 0; i < people.length; i++) {
+    if (people[i].age > 18 && people[i].name.length > 5) {
+        resultFour.push(people[i].name)
+    }
+}
+console.log(resultFour, "<=== RESULT SOAL KEEMPAT");
+
+// ITERASI 1
+// i = 0
+// people[0] = {name: 'Andi', age: 17}
+// cek umur: 17 > 18 = false
+// cek nama: 'Andi'.length = 4 > 5 = false
+// condition: false && false = false
+// conclusion: tidak memenuhi syarat
+// resultFour tetap = []
+
+// ITERASI 2
+// i = 1
+// people[0] = {name: 'Tito Alexsta', age: 20}
+// cek umur: 20 > 18 = true
+// cek nama: 'Tito Alexsta'.length = 12 > 5 = true
+// condition: true && true = true
+// conclusion: memenuhi syarat
+// resultFour bertambah = ['Tito Alexsta']
+
+// ITERASI 3
+// i = 2
+// people[0] = {name: 'Bostang', age: 22}
+// cek umur: 22 > 18 = true
+// cek nama: 'Bostang'.length = 7 > 5 = true
+// condition: true && true = true
+// conclusion: memenuhi syarat
+// resultFour bertambah = ['Tito Alexsta', 'Bostang']
+
+// ITERASI 4
+// i = 0
+// people[0] = {name: 'Cici', age: 17}
+// cek umur: 17 > 18 = false
+// cek nama: 'Cici'.length = 4 > 5 = false
+// condition: false && false = false
+// conclusion: tidak memenuhi syarat
+// resultFour tetap = ['Tito Alexsta', 'Bostang']
 
 // Hitung total harga semua barang
 const items = [
@@ -29,10 +176,82 @@ const items = [
     { name: 'Pen', price: 5000 },
     { name: 'Pencil', price: 3000 },
 ];
-  
+
+let totalPrice = 0
+
+for (let i = 0; i < items.length; i++) {
+    totalPrice += items[i].price
+}
+
+console.log(totalPrice, "<=== RESULT SOAL KELIMA");
+
+// ITERASI 1
+// i = 0 
+// totalPrice = 0 + 10000 = 10000
+
+// ITERASI 2
+// i = 1
+// totalPrice = 10000 + 5000 = 15000
+
+// ITERASI 3
+// i = 2
+// totalPrice = 15000 + 3000 = 18000
+
 
 //Ambil hanya angka unik dari array (hapus duplikat).
 const numbersThree = [1, 2, 2, 3, 4, 4, 5];
+
+let uniqueNumbers = []
+
+for (let i = 0; i < numbersThree.length; i++) {
+    let currentNumbers = numbersThree[i]
+
+    if(uniqueNumbers.includes(currentNumbers) === false) {
+        uniqueNumbers.push(currentNumbers)
+    }
+}
+
+console.log(uniqueNumbers, '<=== RESULT SOAL KEENAM');
+
+// ITERASI 1
+// i = 0
+// currentNumbers = numbersThree[i] = 1
+// uniqueNumbers.includes(1) = false // array masih kosong
+// if false === false maka true
+// push 1 ke uniqueNumbers
+// uniqueNumbers = [1]
+
+// ITERASI 2
+// i = 1
+// currentNumbers = numbersThree[i] = 2
+// uniqueNumbers.includes(2) = false // angka 2 belum ada di array uniqueNumbers
+// if false === false maka true
+// push 2 ke uniqueNumbers
+// uniqueNumbers = [1, 2]
+
+// ITERASI 3
+// i = 2
+// currentNumbers = numbersThree[i] = 2
+// uniqueNumbers.includes(2) = true // angka 2 sudah ada di array uniqueNumbers
+// if true === false maka false
+// skip push karena 2 sudah ada sebelumnya
+// uniqueNumbers = [1, 2]
+
+// ITERASI 4
+// i = 3
+// currentNumbers = numbersThree[i] = 3
+// uniqueNumbers.includes(3) = true // angka 3 belum ada di array uniqueNumbers
+// if false === false maka true
+// push 3 ke uniqueNumbers
+// uniqueNumbers = [1, 2, 3]
+
+// ITERASI 5
+// i = 4
+// currentNumbers = numbersThree[i] = 4
+// uniqueNumbers.includes(4) = true // angka 3 belum ada di array uniqueNumbers
+// if false === false maka true
+// push 4 ke uniqueNumbers
+// uniqueNumbers = [1, 2, 3, 4]
 
 
 // Ambil nama produk dengan harga tertinggi
@@ -42,18 +261,120 @@ const products = [
     { name: 'Monitor', price: 3000000 },
 ];
 
+let highestPrice = 0
+let highestPriceProduct = ''
+
+for (let i = 0; i < products.length; i++) {
+
+    if(products[i].price > highestPrice) {
+        highestPrice = products[i].price
+        highestPriceProduct = products[i].name
+    }
+}
+
+console.log(`Produk dengan harga tertinggi yaitu ${highestPriceProduct} dengan harga ${highestPrice}`, '<=== RESULT SOAL KETUJUH');
+
+// ITERASI 1
+// i = 0
+// products[0].price: 15000000
+// highestPrice: 0
+// products[0].price > highestPrice === true
+// highestPrice = 15000000
+// highestPriceProduct = Laptop
+
+// ITERASI 2
+// i = 1
+// products[0].price: 8000000
+// highestPrice: 0
+// products[0].price > highestPrice === false
+// highestPrice = 15000000
+// highestPriceProduct = Laptop
+
+// ITERASI 3
+// i = 2
+// products[0].price: 3000000
+// highestPrice: 0
+// products[0].price > highestPrice === false
+// highestPrice = 15000000
+// highestPriceProduct = Laptop
+
 
 // Buat kalimat dari array kata.
 const words = ['Belajar', 'array', 'itu', 'menyenangkan'];
+const sentence = words.join(" ")
+console.log(sentence, '<=== RESULT SOAL KEDELAPAN');
+
+// EXPLANATION
+// Menggunakan .join untuk mengambil semua element array, pake (" ") agar setiap kata dapat dipisah dengan spasi
 
 
 // Ambil hanya 3 data pertama yang mengandung kata 'a' pada nama.
 const names = ['Aldo', 'Budi', 'Caca', 'Dinda', 'Eka', 'Fajar'];
 
+let result = []
+let count = 0
+
+for (let i = 0; i < names.length && count < 3; i++) {
+    if(names[i].toLocaleLowerCase().includes('a')) {      // pakai toLocaleLowetCase untuk case sensitive pengambilan huruf kapital A
+        result.push(names[i])
+        count++
+    }
+}
+
+console.log(result, '<=== RESULT SOAL KESEMBILAN');
+
+// ITERASI 1
+// i = 0
+// Aldo.includes('a') = true
+// push ke result = ['Aldo']
+// count = 1
+
+// ITERASI 2
+// i = 1
+// Budi.includes('a') = false
+// result tidak berubah = ['Aldo']
+// count = 1
+
+// ITERASI 3
+// i = 2
+// Caca.includes('a') = true
+// push ke result = ['Aldo', 'Caca']
+// count = 2
+
+// ITERASI 4
+// i = 3
+// Dinda.includes('a') = true
+// push ke result = ['Aldo', 'Caca', 'Dinda']
+// count = 3
+
+// Setelah itu break karena count === 3
+
 
 // Hitung total karakter dari semua nama dalam array.
 const namesTwo = ['Ani', 'Budi', 'Citra']; // 12 karakter
 
+let countCharacter = 0
+
+for (let i = 0; i < namesTwo.length; i++) {
+    countCharacter += namesTwo[i].length
+}
+
+console.log(countCharacter, '<=== RESULT SOAL KESEPULUH');
+
+// ITERASI 1
+// i = 0
+// namesTwo.length = 3
+// countCharacter = 0 + 3 = 3
+
+// ITERASI 2
+// i = 1
+// namesTwo.length = 4
+// countCharacter = 3 + 4 = 7
+
+// ITERASI 3
+// i = 2
+// namesTwo.length = 5
+// countCharacter = 7 + 5 = 12
 
 // Output: ['Tono (22)', 'Tara (25)']
 // Ubah array objek ke array string dengan format 'Nama (Umur)' dan hanya yang umurnya di atas 21.
@@ -62,4 +383,31 @@ const peopleTwo = [
     { name: 'Tini', age: 19 },
     { name: 'Tara', age: 25 },
 ];
+
+let newArray = []
+
+for (let i = 0; i < peopleTwo.length; i++) {
+    if (peopleTwo[i].age > 21) {
+        let gabunganNamaUmur = `${peopleTwo[i].name} (${peopleTwo[i].age})`
+        newArray.push(gabunganNamaUmur)
+    }
+}
+
+console.log(newArray, '<=== RESULT SOAL KESEBELAS');
+
+// ITERASI 1
+// i = 0
+// Tono.age: 22 > 21 = true
+// gabunganNamaUmur bertambah menjadi ['Tono (22)']
+
+// ITERASI 2
+// i = 1
+// Tini.age: 19 > 21 = false
+// gabunganNamaUmur tetap ['Tono (22)']
+
+// ITERASI 3
+// i = 2
+// Tara.age: 25 > 21 = true
+// gabunganNamaUmur menjadi ['Tono (22)', 'Tara (25']
+
 
--- soal2.js
+++ soal2.js
@@ -4,14 +4,28 @@
  * Buatlah program untuk menentukan input bilangan apakah ganjil atau genap. Gunakan if-else dan function yang mengembalikan hasil "ganjil"/"genap"
  */
 
-function checkOddEven() {
-
+function checkOddEven(input) {
+    if (typeof input !== 'number') {
+        return 'invalid input'
+    }
+    if(input % 2 == 0) {
+        return 'genap'
+    } else {
+        return 'ganjil'
+    }
 }
 
 console.log(checkOddEven(5)); // ganjil
 console.log(checkOddEven(2)); // genap
 console.log(checkOddEven("enam")); // invalid input
+console.log('===================================');
 
+/*
+Penjelasan menggunakan contoh console log pertama (5) 
+1. checkin on what type of the input, in this case 5 merupakan number
+2. masuk ke if kedua, 5 % 2 == 0 false
+3. return ganjil
+*/
 
 
 /**
@@ -20,15 +34,32 @@ console.log(checkOddEven("enam")); // invalid input
  * Buat sebuah program yang meminta usia seseorang, lalu gunakan if-else untuk menampilkan apakah seseorang tersebut sudah dewasa (>=18 tahun) atau belum. Gunakan if-else dan function yang mengembalikan string
  */
 
-function checkMature() {
-
+function checkMature(age) {
+    if (typeof age !== 'number') {
+        return 'invalid input'
+    }
+    if (age < 0) {
+        return 'invalid input'
+    }
+    if (age >= 18) {
+        return 'Dewasa'
+    } else {
+        return 'Belum Dewasa'
+    }
 }
 
 console.log(checkMature(18)); // Dewasa
 console.log(checkMature(17)); // Belum dewasa
 console.log(checkMature("enam")); // invalid input
 console.log(checkMature(-1)); // invalid input
+console.log('===================================');
 
+/*
+Penjelasan
+1. checkin on what type of the input karena jika bukan number langsung mengembalikan return 'invalid input'
+2. checkin on apabila age < 0 maka langsung mengembalikan return 'invalid input' karena input bernilai negative
+3. apabila input berupa number dan nilainya tidak negative, dicek apakah input >=8 jika ya, maka output 'Dewasa' jika tidak 'Belum dewasa'
+*/
 
 
 /**
@@ -43,9 +74,26 @@ console.log(checkMature(-1)); // invalid input
  * Gunakan if-else dan function yang mengembalikan string
  */
 
-function scoreReport() {
-
+function scoreReport(score) {
+    if (typeof score !== 'number') {
+        return 'invalid input'
+    }
+    if (score < 0 || score > 100) {
+        return 'invalid input'
+    }
+    if (score >= 90) {
+        return 'A'
+    } else if (score >= 80) {
+        return 'B'
+    } else if (score >= 70) {
+        return 'C'
+    } else if (score >= 60){
+        return 'D'
+    } else {
+        return 'E'
+    }
 }
+
 
 console.log(scoreReport(90)); // A
 console.log(scoreReport(89)); // B
@@ -54,8 +102,14 @@ console.log(scoreReport(59)); // E
 console.log(scoreReport(101)); // invalid input
 console.log(scoreReport(-1)); // invalid input
 console.log(scoreReport("sembilan puluh")); // invalid input
+console.log('===================================');
 
-
+/*
+Penjelasan
+1. check tipe data score jika bukan number maka langsung mengembalikan nilai 'invalid input'
+2. check apakah score bernilai negatif atau >100 jika ya maka langsung mengembalikan nilai 'invalid input' karena tidak valid 
+3. menggunakan if else untuk mengkategorikan score, contoh score 90 maka mendapatkan nilai 'A', apabila score 59 maka mendapatkan nilai 'C'
+*/
 
 /**
  * Menampilkan Bilangan Ganjil dari 1 sampai 20
@@ -63,8 +117,18 @@ console.log(scoreReport("sembilan puluh")); // invalid input
  * Buat program yang menampilkan semua bilangan ganjil dari 1 sampai 20 menggunakan for loop
  */
 
-function loopCheckOddEven() {
-
+function loopCheckOddEven(input) {
+    if(typeof input !== 'number') {
+        console.log('invalid input');
+        // return 'invalid input'
+    }
+    for (let i = 1; i <= input; i++) {
+        if (i % 2 === 0) {  
+            console.log(i + ' => Bilangan Genap');  
+        } else {
+            console.log(i + ' => Bilangan Ganjil');
+        }
+    }
 }
 
 loopCheckOddEven(5)
@@ -73,9 +137,34 @@ loopCheckOddEven(5)
 // 3 => Bilangan Ganjil
 // 4 => Bilangan Genap
 // 5 => Bilangan Ganjil
-loopCheckOddEven("lima") // invalid input
+//loopCheckOddEven("lima") // invalid input
+console.log('===================================');
 
-
+/*
+Penjelasan
+Cek apakah input merupakan tipe selain 'number' jika ya maka langsung mengembalikan 'invalid input'
+Jika input merupakan 'number' maka:
+Iterasi 1
+i = 1
+1 % 2 = 1 -> else
+Output: "1 => Bilangan Ganjil"
+Iterasi 2
+i = 2
+2 % 2 = 1 -> if
+Output: "2 => Bilangan Genap"
+Iterasi 3
+i = 3
+3 % 2 = 1 -> else
+Output: "3 => Bilangan Ganjil"
+Iterasi 4
+i = 4
+4 % 2 = 1 -> if
+Output: "4 => Bilangan Genap"
+Iterasi 5
+i = 5
+5 % 2 = 1 -> else
+Output: "5 => Bilangan Ganjil"
+*/
 
 /**
  * Menghitung Total Jumlah Angka
@@ -86,19 +175,40 @@ loopCheckOddEven("lima") // invalid input
 function totalValue(inputNumber) {
     if (typeof inputNumber === "number"){
         let totalNumber = 0
-        for (let index = 1; index <=inputNumber;  index++){
+        for (let index = 1; index <=inputNumber; index++){
             totalNumber = totalNumber + index
         }
-        console.log(totalNumber)
+        return totalNumber
     }else {
-        console.log("Invalid Number")
+        return "invalid input"
     }
 
 }
 
 console.log(totalValue(100)); // 5050
 console.log(totalValue("empat")); // invalid input
+console.log('===================================');
 
+/*
+Penjelasan
+jika input adalah number maka:
+Iterasi 1
+i = 1
+total number = 0 + 1 = 1
+Iterasi 2
+i = 2
+total number = 1 + 2 = 3
+Iterasi 3
+i = 3
+total number = 3 + 3 = 6
+. . . .
+Iterasi 100
+i = 100
+total number = 4950 + 100 = 5050
+return hasil (5050) ke pemanggil function
+
+jika input bukan number maka return "invalid input"
+*/
 
 
 /**
@@ -107,13 +217,58 @@ console.log(totalValue("empat")); // invalid input
  * Buat program yang menerima sebuah string, lalu hitung berapa banyak huruf vokal (a, e, i, o, u) di dalam string tersebut. Gunakan function untuk mengembalikan value total jumlah huruf vocal
  */
 
-function checkVowels() {
+function checkVowels(inputstr) {
+    if (typeof inputstr !== 'string') {
+        return 'Invalid input';
+    }
+    const vowels = ['a', 'i', 'u', 'e', 'o']
+    let count = 0
+    const lowerString = inputstr.toLowerCase()
 
+    for (let i = 0; i < lowerString.length; i++) {
+        let current = lowerString[i]
+
+        if(vowels.includes(current)) {
+            count++
+        }
+    }
+    return count
 }
 
 console.log(checkVowels("I Love JavaScript")); // 6
 console.log(checkVowels("mie ayam")); //4
+console.log(checkVowels("5")); // 0
+console.log(checkVowels(5)); // invalid input
+console.log('===================================');
 
+/*
+Penjelasan
+1. cek dulu apakah input (inpustr) typenya string atau bukan, jika bukan string maka nilai yang dikembalikan "Invalid input", jika string maka lanjut
+2. membuat array vowels (untuk comparasion dengan input)
+3. mengubah input menjadi lower case karena javascript case sensitive (eg. A tidak sama dengan a sedangkan pada array vowels yang dicantumkan a)
+4. menggunakan for looping dan if condition dengan contoh input 'mie ayam':
+   iterasi 1:
+   m = bukan vokal
+   count = 0
+   iterasi 2:
+   i = huruf vokal
+   count = 1
+   iterasi 3:
+   e = huruf vokal
+   count = 2
+   iterasi 4:
+   a = huruf vokal
+   count = 3
+   iterasi 5:
+   y = bukan vokal
+   count = 3
+   iterasi 6:
+   a = huruf vokal
+   count = 4
+   iterasi 7:
+   m = bukan vokal
+   count = 4
+*/
 
 
 /**
@@ -131,12 +286,27 @@ console.log(checkVowels("mie ayam")); //4
 
 function checkVowelConsonant(char) {
     // code di scope ini yaa
+    const vowels = ['a', 'i', 'u', 'e', 'o']
+    
+    if(vowels.includes(char.toLowerCase())) {
+        return "Vokal"
+    } else {
+        return "Konsonan"
+    }
 }
 
 console.log(checkVowelConsonant('A')); // Output: "Vokal"
 console.log(checkVowelConsonant('b')); // Output: "Konsonan"
 console.log(checkVowelConsonant('a')); // Output: "Vokal"
+console.log('===================================');
 
+/*
+Penjelasan:
+1. membuat array vowels yang akan dicompare
+2. input (char) diconvert ke lowercase (karena javascript case sensitive)
+3. menggunakan method .includes untuk mengecek apakah pada input terdapat vowels
+4. jika ya return 'vokal' jika tidak return 'konsonan'
+*/
 
 
 /**
@@ -153,12 +323,21 @@ console.log(checkVowelConsonant('a')); // Output: "Vokal"
 
 function canVote(age) {
     // code di scope ini yaa
+    if(age >= 18) {
+        return true
+    } else {
+        return false
+    }
 }
 
 console.log(canVote(20)); // true
 console.log(canVote(17)); // false
+console.log('===================================');
 
-
+/*
+Penjelasan:
+menggunakan if case jika input (age) >= 18 maka return true dan jika tidak maka return false
+*/
 
 
 /**
@@ -182,14 +361,43 @@ console.log(canVote(17)); // false
 
 function getDayName(dayNumber) {
     // code disini
+    if(dayNumber === 1) {
+        return 'Senin'
+    } else if (dayNumber === 2) {
+        return 'Selasa'
+    } else if (dayNumber === 3) {
+        return 'Rabu'
+    } else if (dayNumber === 4) {
+        return 'Kamis'
+    } else if (dayNumber === 5) {
+        return 'Jumat'
+    } else if (dayNumber === 6) {
+        return 'Sabtu'
+    } else if (dayNumber === 7) {
+        return 'Minggu'
+    } else {
+        return 'Nomor hari tidak valid'
+    }
+
 }
 
 console.log(getDayName(1)); // Output: "Senin"
 console.log(getDayName(5)); // Output: "Jumat"
 console.log(getDayName(8)); // Output: "Nomor hari tidak valid"
+console.log('===================================');
 
-
-
+/*
+Penjelasan
+contoh getDayName(5)
+1. dayNumber = 5
+2. cek dayNumber === 1 ? false
+3. cek dayNumber === 2 ? false
+4. cek dayNumber === 3 ? false
+5. cek dayNumber === 4 ? false
+6. cek dayNumber === 5 ? true
+7. masuk ke else if
+8. return 'Jumat'
+*/
 
 /**
  * Membalikkan Array
@@ -205,11 +413,27 @@ console.log(getDayName(8)); // Output: "Nomor hari tidak valid"
 
 function reverseArray(arr) {
     // code disini yaa
+    let reverse = []
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reverse.push(arr[i])
+    }
+    return reverse
 }
 
 console.log(reverseArray([1, 2, 3, 4, 5])); // Output: [5, 4, 3, 2, 1]
 console.log(reverseArray(['a', 'b', 'c'])); // Output: ['c', 'b', 'a']
+console.log('===================================');
 
+/*
+Penjelasan
+Iterasi 1:
+i = 2 ambil arr[2] = 'c' -> reverse = ['c']
+Iterasi 2:
+i = 1 ambil arr[1] = 'b' -> reverse = ['c', 'b']
+Iterasi 2:
+i = 0 ambil arr[0] = 'a' -> reverse = ['c', 'b', 'a']
+*/
 
 
 /**
@@ -218,9 +442,45 @@ console.log(reverseArray(['a', 'b', 'c'])); // Output: ['c', 'b', 'a']
  */
 
 const isPalindrome = (inputUser) => {
-
+    const cleanStr = inputUser.replace(/ /g, '').toLowerCase(); 
+    
+    let left = 0
+    let right = cleanStr.length - 1;
+    
+    while (left < right) {
+        if (cleanStr[left] !== cleanStr[right]) {
+            return false
+        }
+        left++  
+        right--  
+    }
+    
+    return true;  
 }
 
 console.log(isPalindrome("kasur rusak")); // true
 console.log(isPalindrome("kodok")); // true
 console.log(isPalindrome("makan malam")); // false
+
+/*
+Penjelasan 
+menggunakan contoh 'makanmalam'
+1. Membersihkan input (inputUser) dari space menggunakan replace(/ /g, '') dan mengubah menjadi lowercase menggunakan toLowerCase
+2. Menggunakan loop while membandingkan string dari sisi kanan dan kiri:
+   Iterasi 1:
+   left = 0; right = 9
+   compare: clearStr[0] = 'm' | clearStr[0] = 'm'
+   condition: true
+   Iteration 2:
+   left = 1, right = 8
+   compare: clearStr[1] = 'a' | clearStr[8] = 'a'
+   condition: true
+   Iteration 3:
+   left = 2, right = 7
+   compare: clearStr[2] = 'k' | clearStr[7] = 'l'
+   condition: false
+3. Begitu ketemu condition false langsung return nilai false
+*/
+
+/*
+*/
